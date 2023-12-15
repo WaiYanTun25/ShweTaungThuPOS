51e0c3ed143216e66f1fe330a52cf7b68e182035
Wai Yan Tun <waiyantun@Wais-MacBook-Pro.local>
2023-12-15 22:26:50 +0630

    add migration tables items, item_price, suppliers, customers

---
+++ database/migrations/2023_12_15_151955_create_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('item_code');
+            $table->integer('category_id');
+            $table->integer('supplier_id');
+            $table->string('item_name');
+            $table->integer('standard_cost');
+            $table->integer('reorder_level');
+            $table->integer('reorder_period');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
---
+++ database/migrations/2023_12_15_154802_create_suppliers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone_number');
+            $table->text('address');
+            $table->string('township');
+            $table->string('city');
+            $table->timestamp('join-date');
+            $table->timestamp('deleted_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('suppliers');
+    }
+};
