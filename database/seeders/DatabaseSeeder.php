c7731fd616bbba117985428c4b0c9191c0c74a50
Wai Yan Tun <waiyantun@Wais-MacBook-Pro.local>
2023-12-23 00:32:50 +0630

    add Seeder for Item, supplier, branch and inventories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleSeeder::class);
         $this->call(AdminUserSeeder::class);
         $this->call(CashierSeeder::class);
+        $this->call(ItemSeeder::class);
     }
 }
--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -2,7 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Inventory;
+use App\Models\Item;
+use App\Models\ItemUnitDetail;
+use App\Models\Supplier;
+use App\Models\Unit;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,12 +18,34 @@ class ItemSeeder extends Seeder
      */
     public function run(): void
     {
+        $categories = [
+            [
+                "name" => 'အချိုရည်',
+                "prefix" => 'A'
+            ],
+            [
+                "name" => 'အိမ်သုံးအဆောင်',
+                "prefix" => 'E'
+            ],
+            [
+                "name" => 'စားသောက်ကုန်',
+                "prefix" => 'S'
+            ],
+        ];
+
+        foreach((array)$categories as $data)
+        {
+            $category = new Category();
+            $category->name = $data['name'];
+            $category->prefix = $data['prefix'];
+            $category->save();
+        }
+        
         $itemData = [
             [
-                "category_id" => 2,
                 "category_id" => 1,
                 "supplier_id" =>  2,
-                "item_name" => "test1",
+                "item_name" => "item 1",
                 "unit_detail" => [
                     [
                         "unit_id" => 1,
@@ -31,7 +58,7 @@ class ItemSeeder extends Seeder
                     ],
                     [
                         "unit_id" => 2,
-                        "rate" => 10,
+                        "rate" => 6,
                         "vip_price" => 15000,
                         "retail_price" => 12000,
                         "wholesale_price" => 11000,
@@ -39,11 +66,162 @@ class ItemSeeder extends Seeder
                         "reorder_period" => 25
                     ]
                 ]
+            ],
+            [
+                "category_id" => 1,
+                "supplier_id" =>  2,
+                "item_name" => "item 2",
+                "unit_detail" => [
+                    [
+                        "unit_id" => 1,
+                        "rate" => 1,
+                        "vip_price" => 2500,
+                        "retail_price" => 2400,
+                        "wholesale_price" => 2100,
+                        "reorder_level" => 6,
+                        "reorder_period" => 10
+                    ],
+                    [
+                        "unit_id" => 2,
+                        "rate" => 6,
+                        "vip_price" => 25000,
+                        "retail_price" => 24000,
+                        "wholesale_price" => 21000,
+                        "reorder_level" => 20,
+                        "reorder_period" => 25
+                    ]
+                ]
             ]
         ];
 
-        $unitData = [
-            
-        ]
+        foreach((array)$itemData as $data)
+        {
+            $item = new Item();
+            $item->category_id = $data['category_id'];
+            $item->supplier_id = $data['supplier_id'];
+            $item->item_name = $data['item_name'];
+            $item->save();
+
+            foreach((array)$data['unit_detail'] as $detail)
+            {
+                $itemUnitDetail = new ItemUnitDetail();
+                $itemUnitDetail->item_id = $item->id;
+                $itemUnitDetail->rate = $detail['rate'];
+                $itemUnitDetail->unit_id = $detail['rate'];
+                $itemUnitDetail->vip_price = $detail['vip_price'];
+                $itemUnitDetail->retail_price = $detail['retail_price'];
+                $itemUnitDetail->wholesale_price = $detail['wholesale_price'];
+                $itemUnitDetail->reorder_level = $detail['reorder_level'];
+                $itemUnitDetail->reorder_period = $detail['reorder_period'];
+                $itemUnitDetail->save();
+            }
+        }
+
+        $unitData = ['ဗူး', '၆ ကဒ်', '၁၂ ကဒ်'];
+
+
+        foreach($unitData as $data)
+        {
+            $unit = new Unit();
+            $unit->name = $data;
+            $unit->save();
+        }
+
+        $inventoryData = [
+            [
+                'branch_id' => 1,
+                'item_id' => 1,
+                'unit_id' => 2,
+                'quantity' => 10
+            ],
+            [
+                'branch_id' => 1,
+                'item_id' => 1,
+                'unit_id' => 1,
+                'quantity' => 100
+            ],
+            [
+                'branch_id' => 1,
+                'item_id' => 2,
+                'unit_id' => 1,
+                'quantity' => 50
+            ],
+            [
+                'branch_id' => 1,
+                'item_id' => 2,
+                'unit_id' => 2,
+                'quantity' => 30
+            ],
+            [
+                'branch_id' => 2,
+                'item_id' => 1,
+                'unit_id' => 2,
+                'quantity' => 10
+            ],
+            [
+                'branch_id' => 2,
+                'item_id' => 1,
+                'unit_id' => 1,
+                'quantity' => 100
+            ],
+            [
+                'branch_id' => 2,
+                'item_id' => 2,
+                'unit_id' => 1,
+                'quantity' => 50
+            ],
+            [
+                'branch_id' => 2,
+                'item_id' => 2,
+                'unit_id' => 2,
+                'quantity' => 30
+            ],
+        ];
+
+        foreach((array)$inventoryData as $data)
+        {
+            $inventory = new Inventory();
+            $inventory->branch_id = $data['branch_id'];
+            $inventory->item_id = $data['item_id'];
+            $inventory->unit_id = $data['unit_id'];
+            $inventory->quantity = $data['quantity'];
+            $inventory->save();
+        }
+
+        $supplierData = [
+            [
+                "name" => "အောင်ကောင်းစည်1",
+                "phone_number" => "0996939693",
+                "address" => "၆၀ ဘီ ကမ်းနားလမ်း အလုံ",
+                "township" => "ကြည့်မြင်တိုင်",
+                "city" => "Yangon"
+            ],
+            [
+                "name" => "အောင်ကောင်းစည်2",
+                "phone_number" => "0996939693",
+                "address" => "၆၀ ဘီ ကမ်းနားလမ်း အလုံ",
+                "township" => "ကြည့်မြင်တိုင်",
+                "city" => "Yangon"
+            ],
+            [
+                "name" => "အောင်ကောင်းစည်3",
+                "phone_number" => "0996939693",
+                "address" => "၆၀ ဘီ ကမ်းနားလမ်း အလုံ",
+                "township" => "ကြည့်မြင်တိုင်",
+                "city" => "Yangon"
+            ],
+        ];
+
+        foreach((array)$supplierData as $data)
+        {
+            $supplier = new Supplier();
+            $supplier->name = $data['name'];
+            $supplier->phone_number = $data['phone_number'];
+            $supplier->address = $data['address'];
+            $supplier->township = $data['township'];
+            $supplier->city = $data['city'];
+            $supplier->save();
+        }
+
     }
 }
